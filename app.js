0f03ecf5a5a4c3095673e99c83c2e7ed028d9447
iqbal <asifmai@hotmail.com>
2020-04-18 11:25:04 +0500

    'update'

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const connectDb = require('./helpers/connectdb');
+
+// Connect to MongoDB
+connectDb();
 
 // Initialize App
 const app = express();
